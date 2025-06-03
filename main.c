1ca1ca768c2ca4b27746fabf391c7ff2d310efc8
Renan <machadosantosrenan@gmail.com>
2025-06-03 20:57:35 -0300

    Added Some Features

--- main.c
+++ main.c
@@ -3,78 +3,6 @@
 #include <math.h>
 #include <string.h>
 #include "packages/funcs.c"
-#include <stdbool.h>
-
-/*
-Adiciona novos produtos até que o usuário deseje parar!
-*/
-int adicionar_produtos() {
-    int capacidade = 5;
-    struct Produto *produtos = NULL;
-    produtos = malloc(capacidade * sizeof(struct Produto));
-    if (produtos == NULL) {
-        perror("Erro ao alocar a memória!");
-        return -1;
-    }
-    int i = 0;
-    char n[50];
-    while (true) {
-        
-        if (i == capacidade) {
-            capacidade *= 2;
-            struct Produto *temp = realloc(produtos, capacidade * sizeof(struct Produto));
-            if (temp == NULL) {
-                perror("Erro ao realocar a memória!");
-                free(produtos);
-                return -1;
-            }
-            produtos = temp;
-        }
-
-        printf("Adicione um novo produto!\n");
-        printf("Digite o nome do produto: \n");
-        fgets(n, sizeof n - 1, stdin);
-        n[strlen(n) - 1] = '\0';
-        printf("Digite o preço do produto: R$");
-        double p;
-        scanf("%lf", &p);
-        printf("\nDigite a quantidade em estoque do produto: ");
-        int q;
-        scanf("%d", &q);
-        printf("\nProcesando Produto!\n");
-
-        printf("-----------------------------------------------\n");
-        printf("Produto adicionado!\n");
-        produtos[i] = novo_produto(n, p, q);
-        printf("-----------------------------------------------\n");
-        printf("Deseja adicionar outro produto? [1] S \\ [2] N\n");
-        printf("\n>_: ");
-        int ram;
-        scanf("%d", &ram);
-
-        if (ram != 1) {
-            break;
-        }
-        i++;
-        getchar();
-    }
-
-    printf("Mostrando todos os produtos!\n");
-    
-
-    for(int j = 0; j < i + 1; j++) {
-        struct Produto prod = produtos[j];
-        printf("-----------------------------------------------\n");
-        printf("Nome: %s\nPreco: R$%.2lf\nQtd.: %d\nCód.: %d\n", prod.nome, prod.preco, prod.quantidade, prod.codigo);
-        printf("-----------------------------------------------\n");
-    }
-    free(produtos);
-    return 0;
-}
-
-
-
-
 
 int main() {
 
@@ -105,10 +33,19 @@ int main() {
     }
 
     char exec_mode = fgetc(exec_info);
+    fclose(exec_mode);
     
     if (exec_mode == 'c') {
         printf("CAIXA\n");
     } else {
-        printf("ADMIN\n");
+        struct Produto* produtos = adicionar_produtos();
+        printf("Mostrando todos os produtos!\n");
+
+        for(int j = 0; j < sizeof(produtos); j++) {
+            struct Produto prod = produtos[j];
+            printf("-----------------------------------------------\n");
+            printf("Nome: %s\nPreco: R$%.2lf\nQtd.: %d\nCod.: %d\n", prod.nome, prod.preco, prod.quantidade, prod.codigo);
+            printf("-----------------------------------------------\n");
+        }
     }
 }
--- packages/funcs.c
+++ packages/funcs.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int cod = 0;
 int user_id = 0;
 typedef struct Produto {
@@ -38,3 +39,58 @@ struct User novo_usuario(char login[100], char senha[20]) {
     return new_user;
 }
 
+/*
+Adiciona novos produtos até que o usuário deseje parar!
+*/
+struct Produto* adicionar_produtos() {
+    int capacidade = 1;
+    struct Produto *produtos = NULL;
+    produtos = malloc(capacidade * sizeof(struct Produto));
+    if (produtos == NULL) {
+        perror("Erro ao alocar a memória!");
+        return NULL;
+    }
+    int i = 0;
+    char n[50];
+    while (1) {
+        
+        if (i == capacidade) {
+            capacidade *= 2;
+            struct Produto *temp = realloc(produtos, capacidade * sizeof(struct Produto));
+            if (temp == NULL) {
+                perror("Erro ao realocar a memória!");
+                free(produtos);
+                return NULL;
+            }
+            produtos = temp;
+        }
+
+        printf("Adicione um novo produto!\n");
+        printf("Digite o nome do produto: \n");
+        fgets(n, sizeof n - 1, stdin);
+        n[strlen(n) - 1] = '\0';
+        printf("Digite o preço do produto: R$");
+        double p;
+        scanf("%lf", &p);
+        printf("Digite a quantidade em estoque do produto: ");
+        int q;
+        scanf("%d", &q);
+        printf("Procesando Produto!\n");
+
+        printf("-----------------------------------------------\n");
+        printf("Produto adicionado!\n");
+        produtos[i] = novo_produto(n, p, q);
+        printf("-----------------------------------------------\n");
+        printf("Deseja adicionar outro produto? [1] S \\ [2] N\n");
+        printf("\n>_: ");
+        int ram;
+        scanf("%d", &ram);
+
+        if (ram != 1) {
+            break;
+        }
+        i++;
+        getchar();
+    }
+    return produtos;
+}
